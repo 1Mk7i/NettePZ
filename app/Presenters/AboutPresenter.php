c71ed0294268e8e6e019f69030bade8a48465f17
1Mk7i <127222921+1Mk7i@users.noreply.github.com>
2023-06-01 17:55:14 +0300

    Single

--- app/Presenters/AboutPresenter.php
+++ app/Presenters/AboutPresenter.php
@@ -6,10 +6,28 @@ namespace App\Presenters;
 
 use Nette;
 
-
 final class AboutPresenter extends Nette\Application\UI\Presenter
 {
-    public function renderShow() : void{
+    private static ?AboutPresenter $instance = null;
+
+    public static function getInstance(): AboutPresenter
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function renderShow(): void
+    {
         $this->getTemplate()->bobrik = 'BOBER';
     }
 }
+
+
